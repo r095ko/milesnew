0df43696d72743ce88df4ef4deef1fa4c29591dd
r095ko <r095ko@gmail.com>
2022-03-17 15:07:33 +0300

    add java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cost = 100;
+        int bonus = 20;
+        int miles = cost / bonus;
+        System.out.println("Вам начислено: " + miles +" бонусных миль(и)");
+
+        }
+}
